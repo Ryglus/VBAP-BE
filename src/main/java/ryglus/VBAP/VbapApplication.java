037c0152957d1a7e996ee9e81347323546fe752a
Ryglus <jirka16.kucera@seznam.cz>
2024-02-08 16:34:27 +0100

    init

---
+++ src/main/java/ryglus/VBAP/VbapApplication.java
@@ -0,0 +1,13 @@
+package ryglus.VBAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ryglus/VBAP/VbapApplicationTests.java
@@ -0,0 +1,13 @@
+package ryglus.VBAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
